42eb05267f213cd4cd2bd0c76687b58905d502e0
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-04-30 23:43:18 +0500

    Same for the Users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(("Username"), max_length=50)
+    email = models.EmailField(("Email"), max_length=100)
+    password = models.CharField(("Password"), max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.username    
