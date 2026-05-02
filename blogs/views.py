516bb46b851bfb9c2275b1fb842ceab302e6d865
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-02 23:53:53 +0500

    Done with view sets

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
+from blogs.models import Blogs
+# from blogs.serializers import BlogSerializer
 # Create your views here.
+
+def BlogView(viewsets.ModelViewSet):
+    queryset = Blogs.objects.all()
+    # serializer_class = BlogSerializer
