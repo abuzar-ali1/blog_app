727068de01fb10c59a372bc337a5ea908b26b5d1
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-02 23:59:32 +0500

    Donw with SErializer

--- blogs/views.py
+++ blogs/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 from blogs.models import Blogs
-# from blogs.serializers import BlogSerializer
+from blogs.serializers import BlogSerializer
+from rest_framework.response import Response
 # Create your views here.
 
-def BlogView(viewsets.ModelViewSet):
+class BlogView(viewsets.ModelViewSet):
     queryset = Blogs.objects.all()
-    # serializer_class = BlogSerializer
+    serializer_class = BlogSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
