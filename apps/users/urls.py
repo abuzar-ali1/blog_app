0ba5a8e884080c83d306bdaac41ef302f340905d
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-05 00:16:51 +0500

    Auth SEtup

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
-from apps.users.views import Userview, UserRegisterView
+from apps.users.views import UserLoginView, Userview, UserRegisterView
 router  = DefaultRouter()
 router.register(r'api/users', Userview, basename='users')
 router.register(r'api/register', UserRegisterView, basename='register')
-
+router.register(r'api/login', UserLoginView, basename='login')
 
 urlpatterns = [
     path('', include(router.urls)),
--- apps/users/views.py
+++ apps/users/views.py
@@ -3,7 +3,9 @@ from rest_framework import viewsets
 from rest_framework.permissions import AllowAny , IsAdminUser , IsAuthenticated , TokenAuthentication
 from apps.users.serializers import UserSerializer
 from .models import User
-
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 
 # Create your views here.
 class Userview(viewsets.ModelViewSet):
@@ -22,8 +24,10 @@ class UserRegisterView(viewsets.ModelViewSet):
 
 
 
-class UserLoginView(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+class UserLoginView(ObtainAuthToken):
+    def post(self , request , *args , **kwargs):
+        serializer = self.serializer_class(data=request.data , context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token , created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
