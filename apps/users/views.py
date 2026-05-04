1374ed0b763375918160926c7942eeb817293588
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-04 20:52:29 +0500

    The User view set and USer Serailizer

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from apps.users.serializers import UserSerializer
+from .models import User
+
 
 # Create your views here.
+class Userview(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
