2b7c33b551c1f51a584a429d66f4884989f6d488
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-05 11:43:04 +0500

    Added the JWt succesfully

--- apps/blogs/views.py
+++ apps/blogs/views.py
@@ -3,12 +3,17 @@ from rest_framework import viewsets
 
 from apps.blogs.models import Blog
 from apps.blogs.serializers import BLogSerailizer
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated , IsAuthenticatedOrReadOnly
+
+
 # Create your views here.
 
 class BlogView(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BLogSerailizer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
 
 
     
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,17 +1,19 @@
+from ctypes.macholib import framework
+
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 from apps.users.views import Userview, UserRegisterView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router  = DefaultRouter()
-router.register(r'api/users', Userview, basename='users')
-router.register(r'api/register', UserRegisterView, basename='register')
+router.register(r'users', Userview, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls)),
+    path('api/register/', UserRegisterView.as_view(), name='register'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 
