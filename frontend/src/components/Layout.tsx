a4f6ff47f1fdf0a09bbbb8023611987f32e59d0d
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-06 23:27:54 +0500

    Done

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen bg-[#0B1120]">
+      <Header />
       
       {/* flex-grow pushes the footer to the bottom */}
       <main className="flex-grow">
