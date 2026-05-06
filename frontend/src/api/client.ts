1f621f5630aa64687547f4a6128bb06c93507a96
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-05-06 22:41:33 +0500

    env issue resolved

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-import process from "process";
 
 
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.API_BASE_URL;
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
